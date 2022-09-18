9ff2fd5e2882e8393568edfb0ba014522925f7c3
agung.hidayat <agung.hidayat@kalbenutritionals.com>
2022-09-18 08:56:56 +0700

    done

--- bmi/lib/views/bmi_result_screen.dart
+++ bmi/lib/views/bmi_result_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bmi/views/bmi_data_screen.dart';
 import 'package:flutter/material.dart';
 
 class BmiResultScreen extends StatelessWidget {
@@ -6,8 +7,83 @@ class BmiResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hasil Hitung BMI")
+      appBar: AppBar(title: Text("Hasil Hitung BMI")),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: const Center(
+                child: Text(
+                  "Result",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: BMICard(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      "Normal",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "18.5",
+                      style: TextStyle(
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "you BMI score is too low you should eat more",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xffec3c66),
+              ),
+              height: 60,
+              child: const Center(
+                child: Text(
+                  "Recalculate BMI",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
